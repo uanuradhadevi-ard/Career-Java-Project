6f7f064ca4b63202b40077a16dbac65464639020
uanuradhadevi-ard <u.anuradhadevi@gmail.com>
2026-06-11 11:24:48 +0000

    added student model class

---
+++ src/main/java/com/project/studentmgmt/model/Student.java
@@ -0,0 +1,12 @@
+package com.project.studentmgmt.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    private Long id;
+    private String name;
+    private String email;
+}
